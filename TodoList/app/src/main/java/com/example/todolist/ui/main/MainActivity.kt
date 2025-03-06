4aa3766f5478e221cf3d1679e7a9c7352e7ad07b
KYM02 <gmyeongmin@gmail.com>
2025-03-06 21:03:44 +0900

    [Feat] dialog 기본 화면 추가
    add dialog frame layout

--- TodoList/app/src/main/java/com/example/todolist/ui/main/MainActivity.kt
+++ TodoList/app/src/main/java/com/example/todolist/ui/main/MainActivity.kt
@@ -17,12 +17,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bottomNavi.setupWithNavController(findNavController(R.id.navi_host_fragment))
-        setBottomNaviColor()
+        setBottomNavi(getColor(R.color.violet_162))
     }
 
-    private fun setBottomNaviColor() {
-        binding.bottomNavi.itemActiveIndicatorColor = ColorStateList.valueOf(getColor(R.color.violet_162))
+    // Bottom navigation 초기 설정
+    private fun setBottomNavi(color : Int) {
+        binding.bottomNavi.setupWithNavController(findNavController(R.id.navi_host_fragment))
+        binding.bottomNavi.itemActiveIndicatorColor = ColorStateList.valueOf(color)
         binding.bottomNavi.itemIconTintList = null
     }
 }
